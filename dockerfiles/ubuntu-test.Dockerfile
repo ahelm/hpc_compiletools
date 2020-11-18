39a47a29e973eea14f7d0681a48112ffb52110de
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-11-18 11:48:25 +0000

    remove user creation from docker file

--- dockerfiles/ubuntu-test.Dockerfile
+++ dockerfiles/ubuntu-test.Dockerfile
@@ -1,6 +1,3 @@
 FROM ubuntu
 
-RUN groupadd -r hpc_user && useradd -r -g hpc_user hpc_user
-USER hpc_user
-
 WORKDIR /project
--- minitest.sh
+++ minitest.sh
@@ -1,2 +1,2 @@
-docker run -v $(pwd):/project $1 bash -c "touch test.txt"
+docker run --user "$(id -u):$(id -g)" -v $(pwd):/project $1 bash -c "touch test.txt"
 rm test.txt
